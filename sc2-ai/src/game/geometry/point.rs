d0537035048d53c34ff33e1b0e47c2f8d119a47b
Ryan Sullivan <ryansully96@googlemail.com>
2025-03-29 12:02:19 +0000

    Make Vec3 a bevy Vec3 wrapper. Added extra functionality

--- sc2-ai/src/game/geometry/point.rs
+++ sc2-ai/src/game/geometry/point.rs
@@ -1,107 +1,122 @@
 use bevy::ecs::component::Component;
+use duplicate::duplicate_item;
 
 #[derive(Component, Default, Clone, Copy, Debug, PartialEq)]
-pub struct Vec3 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-}
+pub struct Vec3(bevy::math::Vec3);
 
 impl Vec3 {
     pub fn new_3d(x: f32, y: f32, z: f32) -> Self {
-        Self { x, y, z }
+        Self(bevy::math::Vec3::new(x, y, z))
     }
 
     pub fn new_2d(x: f32, y: f32) -> Self {
-        Self { x, y, z: 0.0 }
+        Self(bevy::math::Vec3::new(x, y, 0.0))
     }
 }
 
 impl From<sc2_proto::common::Point> for Vec3 {
     fn from(value: sc2_proto::common::Point) -> Self {
-        Self {
-            x: value.x(),
-            y: value.y(),
-            z: value.z(),
-        }
+        Self(bevy::math::Vec3::new(value.x(), value.y(), value.z()))
     }
 }
 
 impl From<Vec3> for sc2_proto::common::Point {
     fn from(value: Vec3) -> Self {
         let mut point = sc2_proto::common::Point::new();
-        point.set_x(value.x);
-        point.set_y(value.y);
-        point.set_z(value.z);
+        point.set_x(value.0.x);
+        point.set_y(value.0.y);
+        point.set_z(value.0.z);
         point
     }
 }
 
 impl From<sc2_proto::common::Point2D> for Vec3 {
     fn from(value: sc2_proto::common::Point2D) -> Self {
-        Self {
-            x: value.x(),
-            y: value.y(),
-            z: 0.0,
-        }
+        Self(bevy::math::Vec3::new(value.x(), value.y(), 0.0))
     }
 }
 
 impl From<Vec3> for sc2_proto::common::Point2D {
     fn from(value: Vec3) -> Self {
         let mut point = sc2_proto::common::Point2D::new();
-        point.set_x(value.x);
-        point.set_y(value.y);
+        point.set_x(value.0.x);
+        point.set_y(value.0.y);
         point
     }
 }
 
-impl std::ops::Add<Vec3> for Vec3 {
-    type Output = Self;
-    #[inline]
-    fn add(self, rhs: Self) -> Self {
-        Self {
-            x: self.x.add(rhs.x),
-            y: self.y.add(rhs.y),
-            z: self.z.add(rhs.z),
-        }
+impl std::ops::Deref for Vec3 {
+    type Target = bevy::math::Vec3;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
-impl std::ops::Add<&Vec3> for Vec3 {
+impl std::ops::DerefMut for Vec3 {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[duplicate_item(
+    Lhs       Rhs;
+    [ Vec3  ] [ Vec3  ];
+    [ Vec3  ] [ &Vec3 ];
+    [ &Vec3 ] [ Vec3  ];
+    [ &Vec3 ] [ &Vec3 ];
+)]
+impl std::ops::Add<Rhs> for Lhs {
     type Output = Vec3;
 
-    fn add(self, rhs: &Vec3) -> Vec3 {
-        self.add(*rhs)
+    fn add(self, rhs: Rhs) -> Vec3 {
+        Vec3(self.0.add(rhs.0))
     }
 }
 
-impl std::ops::Add<&Vec3> for &Vec3 {
+#[duplicate_item(
+    Lhs       Rhs;
+    [ Vec3  ] [ Vec3  ];
+    [ Vec3  ] [ &Vec3 ];
+)]
+impl std::ops::AddAssign<Rhs> for Lhs {
+    fn add_assign(&mut self, rhs: Rhs) {
+        self.0.add_assign(rhs.0);
+    }
+}
+
+#[duplicate_item(
+    Lhs       Rhs;
+    [ Vec3  ] [ Vec3  ];
+    [ Vec3  ] [ &Vec3 ];
+    [ &Vec3 ] [ Vec3  ];
+    [ &Vec3 ] [ &Vec3 ];
+)]
+impl std::ops::Sub<Rhs> for Lhs {
     type Output = Vec3;
 
-    fn add(self, rhs: &Vec3) -> Vec3 {
-        (*self).add(*rhs)
+    fn sub(self, rhs: Rhs) -> Vec3 {
+        Vec3(self.0.sub(rhs.0))
     }
 }
 
-impl std::ops::Add<Vec3> for &Vec3 {
-    type Output = Vec3;
-
-    fn add(self, rhs: Vec3) -> Vec3 {
-        (*self).add(rhs)
+#[duplicate_item(
+    Lhs       Rhs;
+    [ Vec3  ] [ Vec3  ];
+    [ Vec3  ] [ &Vec3 ];
+)]
+impl std::ops::SubAssign<Rhs> for Lhs {
+    fn sub_assign(&mut self, rhs: Rhs) {
+        self.0.sub_assign(rhs.0);
     }
 }
 
-impl std::ops::AddAssign<Vec3> for Vec3 {
-    fn add_assign(&mut self, rhs: Self) {
-        self.x.add_assign(rhs.x);
-        self.y.add_assign(rhs.y);
-        self.z.add_assign(rhs.z);
+impl Vec3 {
+    pub fn length(&self) -> f32 {
+        self.0.length()
     }
-}
 
-impl std::ops::AddAssign<&Vec3> for Vec3 {
-    fn add_assign(&mut self, rhs: &Self) {
-        self.add_assign(*rhs)
+    pub fn distance(&self, other: Self) -> f32 {
+        self.0.distance(other.0)
     }
 }
